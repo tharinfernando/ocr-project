70c8c70f7a7161e0c9488d57ed26b60b69e791b6
tharinfernando <tharinwtk1@gmail.com>
2023-04-23 16:46:51 +0530

    minor change

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,22 @@
     <div>
       <img :src="preview.imageUrl" v-if="preview.imageUrl" />
       <div v-if="extractedText">{{ extractedText }}</div>
+      <div class="copy-icon" @click="copyText">
+        <CopyIcon class="w-2"/>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import Tesseract from "tesseract.js";
+import CopyIcon from "./components/Icons/CopyIcon.vue";
 
 export default {
+  name: "app",
+  components: { 
+    CopyIcon,
+  },
   data() {
     return {
       preview: {
@@ -21,12 +29,20 @@ export default {
     };
   },
   methods: {
-    async processImage(event) {
-      const file = event.target.files[0];
-      this.preview.imageUrl = URL.createObjectURL(file);
-      const result = await Tesseract.recognize(file);
-      this.extractedText = result.text;
-    },
+  async processImage(event) {
+    const file = event.target.files[0];
+    this.preview.imageUrl = URL.createObjectURL(file);
+    const result = await Tesseract.recognize(file);
+    this.extractedText = result.text;
   },
+  copyText() {
+    const textarea = document.createElement("textarea");
+    textarea.value = this.extractedText;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textarea);
+  },
+},
 };
 </script>
